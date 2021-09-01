851558bbb2816c2b1a9a5c77b0da969976829ae8
SebastianC99 <78940347+SebastianC99@users.noreply.github.com>
2021-09-01 13:28:20 -0500

    inicio block

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Character_Controller : MonoBehaviour
 {
-
+    private RaycasHit2D hit;
+    private BoxCollider2D boxCollider;
     public float speed = 4f;
+
+    
 
     Animator anim;
     Rigidbody2D rb2d;
@@ -14,6 +18,7 @@ public class Character_Controller : MonoBehaviour
     void Start(){
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
+        boxcollider = GetComponent<boxCollider2D>();
     }
 
     // Update is called once per frame
@@ -29,6 +34,21 @@ public class Character_Controller : MonoBehaviour
     void FixedUpdate(){
         
         rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
+    }
+
+    hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0,mov.y),Mathf.Abs(mov.y * Time.deltaTime), LayerMask.GetMask("Actor","Blocking"));
+    if(hit.collider == null)
+    {
+        transform.Translate(0, mov.y * Time.deltatime,0);
+    }
+ 
+
+    hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(mov.x,0),Mathf.Abs(mov.x * Time.deltaTime), LayerMask.GetMask("Actor","Blocking"));
+    if(hit.collider == null)
+    {
+        transform.Translate(0, mov.x * Time.deltatime,0,0);
     }
 }
